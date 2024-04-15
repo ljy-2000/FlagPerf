11b410e21d905687a81fb0969fa82b1016066851
ljy-2000 <1822947159@qq.com>
2024-04-15 13:30:21 +0800

    fix model

--- inference/benchmarks/deepseek_7b_mmlu/pytorch/model.py
+++ inference/benchmarks/deepseek_7b_mmlu/pytorch/model.py
@@ -2,6 +2,7 @@ from transformers import AutoModelForCausalLM
 
 
 def create_model(config):
+    model_name = "deepseek-ai/deepseek-llm-7b-base"
     model = AutoModelForCausalLM.from_pretrained(model_name).eval().cuda().float()
     if config.fp16:
         model.half()
